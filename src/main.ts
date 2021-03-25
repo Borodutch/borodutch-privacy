1d3ad4bd89632adf29bb931b1b90053f4a10f4f1
backmeupplz <backmeupplz@gmail.com>
2021-03-25 08:22:52 -0700

    update

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+import App from '@/App.vue'
+import vuetify from '@/plugins/vuetify'
+
+Vue.config.productionTip = true
+
+new Vue({
+  vuetify,
+  render: (h) => h(App),
+}).$mount('#app')
